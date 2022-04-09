c8440dc8970b2d76cb53b38398563094fa4e0efc
HaroldDev <tinaritiziano.dev@gmail.com>
2022-04-09 18:13:11 +0200

    Prova.java file created

---
+++ TestProject/src/it/uniroma3/test/main/Prova.java
@@ -0,0 +1,13 @@
+package it.uniroma3.test.main;
+
+public class Prova {
+	private String str;
+
+	public String getStr() {
+		return this.str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+}
