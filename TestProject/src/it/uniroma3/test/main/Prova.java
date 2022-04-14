54cec20385a166952cd3c817b010562998005a72
HaroldDev <tinaritiziano.dev@gmail.com>
2022-04-14 16:27:32 +0200

    Token Test

--- TestProject/src/it/uniroma3/test/main/Prova.java
+++ TestProject/src/it/uniroma3/test/main/Prova.java
@@ -2,4 +2,12 @@ package it.uniroma3.test.main;
 
 public class Prova {
 	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
 }
